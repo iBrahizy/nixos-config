3df8c11d86921e038fc4c5b325d6593d86a0ba91
ibrahizy <imahmood1710@gmail.com>
2024-08-25 06:20:25 +0100

    dell: blacklist nvidia

--- dell/default.nix
+++ dell/default.nix
@@ -8,23 +8,43 @@
 	modules.virtualisation.enable = true;
 	modules.ssh.enable = true;
 
-	hardware.opengl.enable = true;
-	services.xserver.videoDrivers = [ "nvidia" ];
-	boot.initrd.kernelModules = [ "nvidia" ];
-	boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+	# Blacklisting nvidia, relying on the igpu
+	boot.extraModprobeConfig = ''
+		blacklist nouveau
+		options nouveau modeset=0
+	'';
+  
+	services.udev.extraRules = ''
+		# Remove NVIDIA USB xHCI Host Controller devices, if present
+		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+		# Remove NVIDIA USB Type-C UCSI devices, if present
+		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+		# Remove NVIDIA Audio devices, if present
+		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+		# Remove NVIDIA VGA/3D controller devices
+		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+	'';
+	boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
 
-	hardware.nvidia = {
-		# Modesetting is required.
-		modesetting.enable = true;
-		open = false;
-		nvidiaSettings = true;
-		package = config.boot.kernelPackages.nvidiaPackages.stable;
-	};
-
-	hardware.nvidia.prime = {
-		sync.enable = true;
-
-		intelBusId = "PCI:0:2:0";
-		nvidiaBusId = "PCI:1:0:0";
-	};
+	# This did work, as much as nvidia will work... 
+	# Expected performance in games but choppy performance for normaly use
+	# hardware.opengl.enable = true;
+	# services.xserver.videoDrivers = [ "nvidia" ];
+	# boot.initrd.kernelModules = [ "nvidia" ];
+	# boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+	#
+	# hardware.nvidia = {
+	# 	# Modesetting is required.
+	# 	modesetting.enable = true;
+	# 	open = false;
+	# 	nvidiaSettings = true;
+	# 	package = config.boot.kernelPackages.nvidiaPackages.stable;
+	# };
+	#
+	# hardware.nvidia.prime = {
+	# 	sync.enable = true;
+	#
+	# 	intelBusId = "PCI:0:2:0";
+	# 	nvidiaBusId = "PCI:1:0:0";
+	# };
 }
