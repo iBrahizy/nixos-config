82aa8044427648cd2e17b545f5216eea9c81c7a2
ibrahizy <imahmood1710@gmail.com>
2024-08-25 10:03:47 +0100

    dynamically import home modules and module modules

--- home/default.nix
+++ home/default.nix
@@ -1,17 +1,14 @@
 { ... }:
 
 {
-	imports = [
-		./fish
-		./nvim
-		./starship
-		./git
-		./alacritty
-		./theming
-		./userDirs
-		./cs2
-		./btop
-	];
+	# Not sure why I haven't seen this before? seems like it would be a common pattern
+	# Imports everything in this directory apart from itself (default.nix)
+	imports = let 
+		concatDir = (s: "${./.}/${s}");
+		isNotDefault = (s: s != "default.nix");
+		filesInDir = (builtins.attrNames (builtins.readDir ./.));
+	in
+		builtins.map concatDir (builtins.filter isNotDefault filesInDir);
 
 	home.stateVersion = "24.05";
 }
--- modules/default.nix
+++ modules/default.nix
@@ -1,8 +1,12 @@
 { ... }:
 
 {
-	imports = [
-		./ssh.nix
-		./virtualisation.nix
-	];
+	# Not sure why I haven't seen this before? seems like it would be a common pattern
+	# Imports everything in this directory apart from itself (default.nix)
+	imports = let 
+		concatDir = (s: "${./.}/${s}");
+		isNotDefault = (s: s != "default.nix");
+		filesInDir = (builtins.attrNames (builtins.readDir ./.));
+	in
+		builtins.map concatDir (builtins.filter isNotDefault filesInDir);
 }
