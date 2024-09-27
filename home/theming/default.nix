069f671790948114ccb27d556a9bc1fb4236563a
ibrahizy <imahmood1710@gmail.com>
2024-09-27 18:23:23 +0100

    qt/gtk theming

--- home/theming/default.nix
+++ home/theming/default.nix
@@ -1,13 +1,21 @@
 { pkgs, ... }:
 
 {
-	gtk.enable = true;
-
 	home.pointerCursor = {
 		package = pkgs.phinger-cursors;
 		gtk.enable = true;
 		x11.enable = true;
 		name = "phinger-cursors-dark";
 		size = 24;
+	};
+
+	qt = {
+		enable = true;
+		style.name = "adwaita-dark";
+	};
+
+	gtk = {
+		enable = true;
+		theme.name = "adwaita-dark";
 	};
 }
