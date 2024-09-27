4aec63b03f73823cb8c05584651fc111d569640c
ibrahizy <imahmood1710@gmail.com>
2024-09-27 17:45:33 +0100

    desktop dir change

--- home/userDirs.nix
+++ home/userDirs.nix
@@ -3,7 +3,8 @@
 {
 	xdg.userDirs = {
 		enable = true;
-		desktop = null;
+		# I wanted to keep desktop null but it seems to always create one for me even when i delete it
+		desktop = "${config.home.homeDirectory}/desktop";
 		download = "${config.home.homeDirectory}/downloads";
 		documents = "${config.home.homeDirectory}/documents";
 		music = null;
