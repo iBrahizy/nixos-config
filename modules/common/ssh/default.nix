d6b3c62ddd985d3b64dbf83dcbf6c65fb99c0b08
ibrahizy <imahmood1710@gmail.com>
2024-09-27 17:24:35 +0100

    restructured ssh

--- modules/common/ssh/default.nix
+++ modules/common/ssh/default.nix
@@ -1,39 +1,15 @@
-# I have the exact same configuration for ssh between devices, apart from port
-# the motivation behind the module is so that i can keep that configuration but change the port
-
 { lib, config, ... }:
 
-let
-	cfg = config.modules.ssh;
-in
 {
-	options.modules.ssh = {
-		enable = lib.mkOption {
-			type = lib.types.bool;
-			default = true;
-			description = "Enable custom SSH configuration.";
-		};
-
-		port = lib.mkOption {
-			type = lib.types.int;
-			default = 22;
-			description = "The port that SSH should listen on.";
+	services.openssh = {
+		enable = true;
+		settings = {
+			PasswordAuthentication = false;
+			PermitRootLogin = "no";
 		};
 	};
 
-	config = lib.mkIf cfg.enable {
-		services.openssh = {
-			enable = true;
-			ports = [ cfg.port ];
-			settings = {
-				PasswordAuthentication = false;
-				PermitRootLogin = "no";
-			};
-		};
-
-		users.users."ibrahizy".openssh.authorizedKeys.keyFiles = [
-			./authorized_keys
-		];
-	};
+	users.users."ibrahizy".openssh.authorizedKeys.keyFiles = [
+		./authorized_keys
+	];
 }
-
