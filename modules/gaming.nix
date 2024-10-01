c7f48d60b8ecd9e18bb43f159e922a34b8d3bb7c
ibrahizy <imahmood1710@gmail.com>
2024-10-01 14:05:36 +0100

    flake update

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -6,6 +6,10 @@
 		remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
 		dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
 		localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
+		extraCompatPackages = with pkgs; [
+			unstable.proton-ge-bin
+		];
 	};
 
 	environment.defaultPackages = with pkgs; [ 
