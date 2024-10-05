083db12367f27bb8e6f1419d23565e4781596eea
ibrahizy <imahmood1710@gmail.com>
2024-10-05 22:11:11 +0100

    home pc nvidia

--- hosts/homepc/default.nix
+++ hosts/homepc/default.nix
@@ -1,4 +1,4 @@
-{ modules, ... }:
+{ config, modules, ... }:
 
 {
 	imports = [
@@ -6,6 +6,17 @@
 		/${modules}/desktop
 	];
 	
+	hardware.opengl.enable = true;
+	services.xserver.videoDrivers = [ "nvidia" ];
+
+	hardware.nvidia = {
+		modesetting.enable = true;
+		powerManagement.enable = false;
+		open = false;
+		nvidiaSettings = true;
+		package = config.boot.kernelPackages.nvidiaPackages.stable;
+	};
+
 	system.stateVersion = "24.05";
 	home-manager.users.ibrahizy.home.stateVersion = "24.05";
 }
