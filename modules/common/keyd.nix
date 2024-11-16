27c3f7242271c23cf371785ad9f788d054462149
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2024-11-16 21:11:27 +0000

    more keyd config

--- modules/common/keyd.nix
+++ modules/common/keyd.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
 	services.keyd = {
@@ -6,15 +6,44 @@
 
 		keyboards.default.settings = {
 			main = {
-				"102nd" = "overload(nav, 102nd)";
+				# 102nd is backslash on the uk layout
+				"102nd" = "overload(backslash-overlay, 102nd)";
+
+				# For consistancy with the 60%
+				"`" = "esc";
 			};
 
-			nav = {
-				h = "left";
-				j = "down";
-				k = "up";
-				l = "right";
-			};
+			backslash-overlay = 
+			let
+				range = lib.range 1 9;
+				toFAttribute = (x: { "${x}" = "f${x}"; });
+				f = (x: acc: acc // toFAttribute (toString x));
+				# \ + 1..9 will be mapped to F1..F9
+				fKeysAttributes = lib.fold f {} range;
+			in
+				fKeysAttributes // 
+				{
+					"0" = "f10";
+					"-" = "f11";
+
+					h = "left";
+					j = "down";
+					k = "up";
+					l = "right";
+
+					u = "pagedown";
+					i = "pageup";
+
+					"`" = "`";
+					esc = "`";
+				}; 
+
 		};
+
+		# There is some bug with setting equals equal to something
+		keyboards.default.extraConfig = ''
+			[backslash-overlay]
+			==f12
+		'';
 	};
 }
