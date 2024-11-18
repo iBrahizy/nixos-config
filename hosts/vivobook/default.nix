bc1ebeedcd21fc202cd967b43cafccc7b358a310
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2024-11-18 08:08:42 +0000

    vivobook: power configuration

--- hosts/vivobook/default.nix
+++ hosts/vivobook/default.nix
@@ -9,6 +9,20 @@
 		/${modules}/uni
 	];
 
+	powerManagement.enable = true;
+
+	# I haven't really compared performance with ppd vs tlp
+	# I mostly just want the stop charging threshhold
+	services.power-profiles-daemon.enable = false;
+	services.tlp = {
+		enable = true;
+
+		settings = {
+			# Stop charging at 80%
+			STOP_CHARGE_THRESH_BAT0 = 80; 
+		};
+	};
+
 	system.stateVersion = "24.05";
 	home-manager.users.ibrahizy.home.stateVersion = "24.05";
 }
