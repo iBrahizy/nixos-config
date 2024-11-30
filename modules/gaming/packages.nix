73aab73e10ace4546851c7b27bf746da7b31d39f
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2024-11-30 21:39:20 +0000

    nixos 24.11!

--- modules/gaming/packages.nix
+++ modules/gaming/packages.nix
@@ -2,7 +2,7 @@
 
 {
 	environment.defaultPackages = [
-		pkgs.lumafly
+		pkgs.unstable.lumafly # The stable package has a vunrable version of dotnet lol
 		(pkgs.callPackage /${flakeRoot}/packages/osu-lazer-bin.nix {})
 		pkgs.unstable.r2modman
 	];
