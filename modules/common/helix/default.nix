58b6d79eda18afeb966a319b05d8afd135d2f947
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2024-12-30 15:18:01 +0000

    helix changes :D

--- modules/common/helix/default.nix
+++ modules/common/helix/default.nix
@@ -6,12 +6,17 @@
 		enable = true;
 
 		settings = {
-			theme = "catppuccin_mocha";
+			theme = "my-catppuccin";
 
 			editor = {
+				cursorline = true;
+				true-color = true;
 				idle-timeout = 20;
 				completion-timeout = 20;
 				completion-trigger-len = 1;
+				auto-format = false;
+				color-modes = true;
+				popup-border = "all";
 
 				indent-guides = {
 					render = true;
@@ -28,18 +33,31 @@
 
 			keys = {
 				normal = {
+					A-x = "extend_to_line_bounds";
+					X = "select_line_above";
 					"0" = "goto_line_start";
 					D = [ "collapse_selection" "select_mode" "goto_line_end" "delete_selection" ];
+					"{" = "goto_prev_paragraph";
+					"}" = "goto_next_paragraph";
+					"=" = ":format";
+					"+" = "format_selections";
+				};
+
+				select = {
+					A-x = "extend_to_line_bounds";
+					X = "select_line_above";
 				};
 			};
 		};
 
 		languages = {
 			language = [{
-			name = "nix";
-			indent = { tab-width = 4; unit = "\t"; };
+				name = "nix";
+				indent = { tab-width = 4; unit = "\t"; };
 			}];
 		};
+
+		themes.my-catppuccin = builtins.fromTOML (builtins.readFile ./my-catppuccin.toml);
 	};
 	};
 }
