435a3eb871ebc9c7a5e5efd0aea6dd49d502b736
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2024-12-31 20:57:42 +0000

    osu update again!?

--- packages/osu-lazer-bin.nix
+++ packages/osu-lazer-bin.nix
@@ -8,11 +8,11 @@
 
 let
 	pname = "osu-lazer-bin";
-	version = "2024.1224.1";
+	version = "2025.101.0";
 
 	src = fetchurl {
 		url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-		hash = "sha256-9qZ5XNAII2I4PemNGrWuUCY1Syq5PH3NBc2/Jj5tcoQ=";
+		hash = "sha256-GsnTxVpNk2RXHLET6Ugv0/ZOlq8RUkw2ZXqRjkU+dzw=";
 	};
 
 	meta = {
