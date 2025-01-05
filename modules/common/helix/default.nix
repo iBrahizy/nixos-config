0d8f9c05336e4649e79404e53c5df885c4cb0b81
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2025-01-05 20:24:15 +0000

    helix 25.01

--- modules/common/helix/default.nix
+++ modules/common/helix/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ lib, pkgs, config, ... }:
 
 {
 	
@@ -7,7 +7,21 @@
 			enable = true;
 			defaultEditor = true;
 
-			package = pkgs.unstable.helix;
+			package = (pkgs.helix.overrideAttrs (oldAttrs: rec {
+				version = "25.01";
+
+				src = pkgs.fetchzip {
+					url = "https://github.com/helix-editor/helix/releases/download/${version}/helix-${version}-source.tar.xz";
+					hash = "sha256-HyDsHimDug+8vX0wfon4pK0DEYH5402CDinp3EZpaWs=";
+					stripRoot = false;
+				};
+
+				cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
+				    # name = "${name}-vendor";
+				    inherit src;
+				    outputHash = "sha256-siOrpSgvvdVINAgVFpU77gFHtPes0N1lngQYDYhRWgU=";
+				});
+			}));
 		};
 
 		home.file.".config/helix".source = ./.;
