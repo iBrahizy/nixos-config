cb0aa75e702c94957154c144fe6a9f4d8d9b4914
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2025-05-24 20:28:22 +0100

    changed cs2 senitivity

--- modules/desktop/wm.nix
+++ modules/desktop/wm.nix
@@ -20,7 +20,10 @@
 		};
 	};
 
-	services.desktopManager.plasma6.enable = true;
+	services.desktopManager = {
+		plasma6.enable = true;
+		cosmic.enable = true;
+	};
 
 	environment.defaultPackages = with pkgs; [
 		xclip
