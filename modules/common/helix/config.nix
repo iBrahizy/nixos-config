8350faf33694450dd3fcbfd273b4c7874774061c
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2025-06-09 12:47:06 +0100

    helix: fun changes

--- modules/common/helix/config.nix
+++ modules/common/helix/config.nix
@@ -1,7 +1,41 @@
 { ... }:
 
+let
+	normal_and_select = {
+		"0" = "goto_first_nonwhitespace";
+		"G" = "goto_last_line";
+		"+" = "format_selections";
+		"=" = ":format";
+		"D" = ["collapse_selection" "extend_to_line_end" "delete_selection"];
+		"C" = ["collapse_selection" "extend_to_line_end" "change_selection"];
+		"{" = "goto_prev_paragraph";
+		"}" = "goto_next_paragraph";
+		"Y" = "yank_joined";
+		"A-e" = "move_next_sub_word_end";
+		"A-w" = "move_next_sub_word_start";
+		"A-b" = "move_prev_sub_word_start";
+		"K" = "hover";
+		"A-k" = "keep_selections";
+		"A-c" = "copy_selection_on_next_line";
+
+		space = {
+			"f" = "file_picker_in_current_directory";
+			"F" = "file_picker";
+			"w" = ":w";
+			"k" = "no_op";
+			"e" = "file_browser_in_current_buffer_directory";
+		};
+
+		"]" = {
+			"n" = "@/[0-9]+\\.*[0-9]*<ret>";
+		};
+
+		"[" = {
+			"n" = "@?[0-9]+\\.*[0-9]*<ret>";
+		};	
+	};
+in
 {
-	# theme = "catppuccin_mocha";
 	theme = "catppuccin";
 
 	editor = {
@@ -42,53 +76,24 @@
 	};
 
 	keys = {
-		normal = {
-			"0" = "goto_first_nonwhitespace";
-			"G" = "goto_last_line";
-			"+" = "format_selections";
-			"=" = ":format";
-			"D" = ["collapse_selection" "extend_to_line_end" "delete_selection"];
-			"C" = ["collapse_selection" "extend_to_line_end" "change_selection"];
-			"L" = ["collapse_selection" "extend_to_line_end"]; 
-			"H" = ["collapse_selection" "extend_to_line_start"]; 
+		normal = normal_and_select // {
 			"x" = ["goto_line_start" "select_mode" "goto_line_end_newline"];
 			"X" = ["goto_line_start" "select_mode" "goto_line_end_newline"];
-			"{" = "goto_prev_paragraph";
-			"}" = "goto_next_paragraph";
-			"Y" = "yank_joined";
-			"A-e" = "move_next_sub_word_end";
-			"A-w" = "move_next_sub_word_start";
-			"A-b" = "move_prev_sub_word_start";
-			"K" = "hover";
-			"C-k" = "keep_selections";
-			"A-c" = "copy_selection_on_next_line";
-
-			space = {
-				"f" = "file_picker_in_current_directory";
-				"F" = "file_picker";
-				"w" = ":w";
-				"k" = "no_op";
-				"e" = "file_browser_in_current_buffer_directory";
-			};
-
-			"]" = {
-				"n" = "@/[0-9]+<ret>";
-			};
-
-			"[" = {
-				"n" = "@?[0-9]+<ret>";
-			};
+			"L" = ["collapse_selection" "extend_to_line_end"]; 
+			"H" = ["collapse_selection" "extend_to_line_start"]; 
+			"C-j" = ["extend_to_line_bounds" "delete_selection" "paste_after"];
+			"C-k" = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
 		};
 
-		select = {
+		select = normal_and_select // {
 			"X" = "select_line_above";
 			"{" = "goto_prev_paragraph";
 			"}" = "goto_next_paragraph";
 			"L" = "extend_to_line_end";
 			"H" = "extend_to_line_start";
-			"C-k" = "keep_selections";
-			"K" = "no_op";
-			"G" = "goto_last_line";
+			";" = ["collapse_selection" "normal_mode"];
+			"C-j" = ["extend_to_line_bounds" "delete_selection" "paste_after" "select_mode"];
+			"C-k" = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before" "select_mode"];
 		};
 
 		insert = {
