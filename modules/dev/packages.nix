ba57f2de3a8cc5e6e4c6b3796bc5ab1a5d242a4a
Ibrahim Mahmood-Qureshi <imahmood1710@gmail.com>
2026-05-02 23:35:49 +0100

    include some python packages by default

--- modules/dev/packages.nix
+++ modules/dev/packages.nix
@@ -3,11 +3,23 @@
 {
 	environment.defaultPackages = with pkgs; [
 		gcc
-		python3
-		python312Packages.python-lsp-server
+		# python3
+		# python312Packages.python-lsp-server
+		(python312.withPackages (ps: with ps; [
+			python-lsp-server
+			numpy
+			scipy
+			jupyterlab
+			pandas
+			statsmodels
+			scikit-learn
+			pytest
+			pypdf
+		]))
 		rustup
 		ghc
 		haskell-language-server
 		cabal-install
+		graphviz
 	];
 }
